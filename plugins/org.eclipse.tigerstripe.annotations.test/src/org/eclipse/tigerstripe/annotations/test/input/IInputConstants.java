ff90735410859b6d7acf0823135d7501c0198eb7
edillon <edillon>
2008-01-17 18:54:26 +0000

    Initial set of JUnit test cases for the annotations framework.

---
+++ plugins/org.eclipse.tigerstripe.annotations.test/src/org/eclipse/tigerstripe/annotations/test/input/IInputConstants.java
@@ -0,0 +1,30 @@
+/*******************************************************************************
+ * Copyright (c) 2008 Cisco Systems, Inc.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Cisco Systems, Inc. - Initial API and Implementation
+ *******************************************************************************/
+package org.eclipse.tigerstripe.annotations.test.input;
+
+/**
+ * A convenience interface defining a bunch of constants used throughout the
+ * test cases.
+ * 
+ * @author erdillon
+ * 
+ */
+public interface IInputConstants {
+
+	public static final int NUMBER_OF_SCHEMES = 2;
+
+	public static final String URI1 = "uri1";
+	public static final String URI2 = "uri2";
+
+	public static final String SCHEME1_ID = "org.eclipse.tigerstripe.annotations.test.scheme1";
+	public static final String SCHEME2_ID = "org.eclipse.tigerstripe.annotations.test.scheme2";
+
+}
---
+++ plugins/org.eclipse.tigerstripe.annotations.test/src/org/eclipse/tigerstripe/annotations/test/input/SchemeSelector1.java
@@ -0,0 +1,40 @@
+/*******************************************************************************
+ * Copyright (c) 2008 Cisco Systems, Inc.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Cisco Systems, Inc. - Initial API and Implementation
+ *******************************************************************************/
+package org.eclipse.tigerstripe.annotations.test.input;
+
+import org.eclipse.tigerstripe.annotations.AnnotationCoreException;
+import org.eclipse.tigerstripe.annotations.IAnnotationSpecification;
+import org.eclipse.tigerstripe.annotations.ISelector;
+
+/**
+ * 	Selector that selects on IInputConstants.URI1
+ * 
+ * @author erdillon
+ *
+ */
+public class SchemeSelector1 implements ISelector {
+
+	public boolean isEnabled(IAnnotationSpecification spec, String URI)
+			throws AnnotationCoreException {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public boolean select(String URI) {
+		return URI.equals(IInputConstants.URI1);
+	}
+
+	public void setContext(Object context) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
