0917d55819641e56c0e4e1131fcc393c2d3c1c62
jstrawn <jstrawn>
2008-03-12 22:11:17 +0000

    222487 Updated legal information

--- misc/tigerstripe-maven-plugin/src/main/java/org/eclipse/tigerstripe/mojo/GenerateMojo.java
+++ misc/tigerstripe-maven-plugin/src/main/java/org/eclipse/tigerstripe/mojo/GenerateMojo.java
@@ -1,3 +1,13 @@
+/*******************************************************************************
+ * Copyright (c) 2008 Cisco Systems, Inc.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Cisco Systems, Inc. - Initial version
+ *******************************************************************************/
 package org.eclipse.tigerstripe.mojo;
 
 import java.io.File;
