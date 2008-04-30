6633cf3425a6e86167ee7071af689e08e29b6400
edillon <edillon>
2008-04-30 15:49:02 +0000

    First update working locally

--- plugins/org.eclipse.tigerstripe.releng/build.sh
+++ plugins/org.eclipse.tigerstripe.releng/build.sh
@@ -15,15 +15,21 @@
 ####
 ## YOU NEED TO ADJUST THESE TO YOUR LOCAL SETTINGS
 ##
-export	ANT_HOME=/auto/tigerstripe/java/apache-ant-1.7.0
-export	JAVA_HOME=/auto/tigerstripe/java/jdk1.6.0_10/
+export	ANT_HOME=/usr/share/ant
+export	JAVA_HOME=/System/Library/Frameworks/JavaVM.framework/Versions/1.5/Home
 export	PATH=${JAVA_HOME}/bin:${PATH}:${ANT_HOME}/bin
 
-## Base classpath for Eclipse Launcher
+## These mimic the environment that would be created by Hudson
+export	BUILD_ID=1
+export	TSDATE=`date "+%Y%m%d%H%M%S" | sed "s/[_-]//g" | awk '{ print substr($1, 0, 8)}'`
+export	TSTIME=`date "+%Y%m%d%H%M%S" | sed "s/[_-]//g" | awk '{ print substr($1, 9, 4)}'`
+
+## Base classpath for Eclipse Launcher	
 export CLASSPATH=pde/org.eclipse.releng.basebuilder/plugins/org.eclipse.equinox.launcher.jar
 
 ## Adding jSch to classpath to enable "scp" task in Ant.
 export CLASSPATH=${CLASSPATH}:lib/jsch-0.1.37.jar
+
 
 ${JAVA_HOME}/bin/java -Xmx1024m -cp ${CLASSPATH} \
 	org.eclipse.core.launcher.Main -application org.eclipse.ant.core.antRunner "$@"
