ab1ae6bd64335b2e4a0f826a6e159fea88faa245
rcraddock <rcraddock>
2008-05-13 16:04:30 +0000

    Added file copyright headers.
    Renamed package audit -> builder for consistency with base

---
+++ plugins/org.eclipse.tigerstripe.workbench.ui.visualeditor.diagram/src/org/eclipse/tigerstripe/workbench/ui/visualeditor/diagram/builder/WVDFileAuditor.java
@@ -0,0 +1,24 @@
+/*******************************************************************************
+ * Copyright (c) 2007 Cisco Systems, Inc.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    R. Craddock (Cisco Systems, Inc.) 
+ *******************************************************************************/
+package org.eclipse.tigerstripe.workbench.ui.visualeditor.diagram.builder;
+
+
+public class WVDFileAuditor extends DiagramFileAuditor  {
+
+	public String getMatchingExtension(){
+		return "vwm";
+	}
+	
+	public String getFileExtension() {
+		return "wvd";
+	}
+
+}
