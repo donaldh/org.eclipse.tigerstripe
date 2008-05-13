b49e7f8bc664ea73b79e762bebe425c66ea208b0
rcraddock <rcraddock>
2008-05-13 16:03:50 +0000

    Added file copyright headers

--- plugins/org.eclipse.tigerstripe.workbench.ui.UML2Import/src/org/eclipse/tigerstripe/workbench/ui/uml2import/internal/ui/wizards/UML2ImportDetailsWizardPage.java
+++ plugins/org.eclipse.tigerstripe.workbench.ui.UML2Import/src/org/eclipse/tigerstripe/workbench/ui/uml2import/internal/ui/wizards/UML2ImportDetailsWizardPage.java
@@ -1,3 +1,13 @@
+/*******************************************************************************
+ * Copyright (c) 2007 Cisco Systems, Inc.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    R. Craddock (Cisco Systems, Inc.) 
+ *******************************************************************************/
 package org.eclipse.tigerstripe.workbench.ui.uml2import.internal.ui.wizards;
 
 import java.io.File;
