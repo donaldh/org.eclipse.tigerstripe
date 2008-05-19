24adbefda43d35c83748db0682658e2a67302aa6
edillon <edillon>
2008-05-19 21:17:39 +0000

    Initial commit for empty test plugins for Annotation Core.

---
+++ plugins/org.eclipse.tigerstripe.annotation.core.test/src/org/eclipse/tigerstripe/annotation/core/test/AllTests.java
@@ -0,0 +1,27 @@
+/*******************************************************************************
+ * Copyright (c) 2008 Cisco Systems, Inc.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Cisco Systems, Inc. - erdillon
+ *******************************************************************************/
+package org.eclipse.tigerstripe.annotation.core.test;
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
+
+public class AllTests {
+
+	public static Test suite() {
+		TestSuite suite = new TestSuite(
+				"Test for org.eclipse.tigerstripe.annotation.core.test");
+		// $JUnit-BEGIN$
+		suite.addTestSuite(NoTest.class);
+		// $JUnit-END$
+		return suite;
+	}
+
+}
---
+++ plugins/org.eclipse.tigerstripe.annotation.core.test/src/org/eclipse/tigerstripe/annotation/core/test/NoTest.java
@@ -0,0 +1,20 @@
+/*******************************************************************************
+ * Copyright (c) 2008 Cisco Systems, Inc.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Cisco Systems, Inc. - erdillon
+ *******************************************************************************/
+package org.eclipse.tigerstripe.annotation.core.test;
+
+import junit.framework.TestCase;
+
+public class NoTest extends TestCase {
+
+	public void testNoTest() {
+		// this is simply an example
+	}
+}
