ec8c8a090541d0dcc8a3a82bbd71fdc50a0411ad
ystrot <ystrot>
2008-05-27 09:56:22 +0000

    Added tests.

--- plugins/org.eclipse.tigerstripe.annotation.core.test/src/org/eclipse/tigerstripe/annotation/core/test/AllTests.java
+++ plugins/org.eclipse.tigerstripe.annotation.core.test/src/org/eclipse/tigerstripe/annotation/core/test/AllTests.java
@@ -20,6 +20,7 @@ public class AllTests {
 				"Test for org.eclipse.tigerstripe.annotation.core.test");
 		// $JUnit-BEGIN$
 		suite.addTestSuite(NoTest.class);
+		suite.addTestSuite(TreeTest.class);
 		suite.addTestSuite(AddRemoveAnnotationTest.class);
 		suite.addTestSuite(QueryTest.class);
 		suite.addTestSuite(ChangeRecordingTest.class);
