07642964deff62fa48ec5fd4f7138c5ec6b9209f
edillon <edillon>
2008-06-15 01:04:09 +0000

    More fixing of build.properties files, hoping to finally fix the pb within annotations in built versions.

--- plugins/org.eclipse.tigerstripe.espace.resources/src/org/eclipse/tigerstripe/espace/resources/core/EMFDatabase.java
+++ plugins/org.eclipse.tigerstripe.espace.resources/src/org/eclipse/tigerstripe/espace/resources/core/EMFDatabase.java
@@ -376,7 +376,7 @@ public class EMFDatabase implements IEMFDatabase {
 	protected void updateResource(IResource resource, boolean added) {
 		if (resource instanceof IFile) {
 			IFile file = (IFile)resource;
-			if (!isLocked() && file.getFileExtension().toLowerCase().equals(".ann")) {
+			if (!isLocked() && file.getFileExtension().toLowerCase().equals("ann")) {
 				getResourceStorage().updateResource(resource, added);
 				doRebuildIndex();
 			}
