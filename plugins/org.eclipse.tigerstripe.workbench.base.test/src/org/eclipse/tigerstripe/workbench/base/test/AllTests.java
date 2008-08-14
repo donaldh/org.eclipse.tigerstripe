a9a6ba202c981f4f5fca11c4decc1a969c8dd2a9
edillon <edillon>
2008-08-14 23:24:30 +0000

    Bug 244219: starting from scratch

--- plugins/org.eclipse.tigerstripe.workbench.base.test/src/org/eclipse/tigerstripe/workbench/base/test/AllTests.java
+++ plugins/org.eclipse.tigerstripe.workbench.base.test/src/org/eclipse/tigerstripe/workbench/base/test/AllTests.java
@@ -13,34 +13,6 @@ package org.eclipse.tigerstripe.workbench.base.test;
 import junit.framework.Test;
 import junit.framework.TestSuite;
 
-import org.eclipse.tigerstripe.workbench.base.test.adapt.TestAdapters;
-import org.eclipse.tigerstripe.workbench.base.test.annotation.TestAnnotationCreationAPI;
-import org.eclipse.tigerstripe.workbench.base.test.builders.TestBasicM1ProjectAuditor;
-import org.eclipse.tigerstripe.workbench.base.test.builders.TestBasicModelProjectAuditor;
-import org.eclipse.tigerstripe.workbench.base.test.facet.BasicFacetTest;
-import org.eclipse.tigerstripe.workbench.base.test.facet.TestFacetResolution;
-import org.eclipse.tigerstripe.workbench.base.test.facet.TestFacetWithAnnotations;
-import org.eclipse.tigerstripe.workbench.base.test.generation.TestM0DeployUndeploy;
-import org.eclipse.tigerstripe.workbench.base.test.generation.TestM1Generation;
-import org.eclipse.tigerstripe.workbench.base.test.generation.TestProjectGenerationBasics;
-import org.eclipse.tigerstripe.workbench.base.test.migration.TestArtifactRefactor;
-import org.eclipse.tigerstripe.workbench.base.test.migration.TestFieldMigration;
-import org.eclipse.tigerstripe.workbench.base.test.migration.TestManagedEntityMigration;
-import org.eclipse.tigerstripe.workbench.base.test.migration.TestModelManager;
-import org.eclipse.tigerstripe.workbench.base.test.model.TestArtifacts;
-import org.eclipse.tigerstripe.workbench.base.test.model.TestFields;
-import org.eclipse.tigerstripe.workbench.base.test.model.TestLiterals;
-import org.eclipse.tigerstripe.workbench.base.test.model.TestMethods;
-import org.eclipse.tigerstripe.workbench.base.test.model.TestTigerstripeWorkspaceNotifications;
-import org.eclipse.tigerstripe.workbench.base.test.profile.TestProfileBasics;
-import org.eclipse.tigerstripe.workbench.base.test.project.TestM0ProjectBasics;
-import org.eclipse.tigerstripe.workbench.base.test.project.TestM0ProjectContents;
-import org.eclipse.tigerstripe.workbench.base.test.project.TestM1ProjectBasics;
-import org.eclipse.tigerstripe.workbench.base.test.project.TestM1ProjectContents;
-import org.eclipse.tigerstripe.workbench.base.test.project.TestModelProjectLifecycle;
-import org.eclipse.tigerstripe.workbench.base.test.project.TestProjectManagement;
-import org.eclipse.tigerstripe.workbench.base.test.startup.TestStartup;
-
 public class AllTests {
 
 	public static Test suite() {
@@ -48,54 +20,54 @@ public class AllTests {
 				"Test for org.eclipse.tigerstripe.workbench.base.test");
 		// $JUnit-BEGIN$
 
-		// Startup Tests
-		suite.addTestSuite(TestStartup.class);
-
-		suite.addTestSuite(TestTigerstripeCore.class);
-
-		// Basic project operations
-		suite.addTestSuite(TestProjectManagement.class);
-		suite.addTestSuite(TestModelProjectLifecycle.class);
-		suite.addTestSuite(TestM1ProjectBasics.class);
-		suite.addTestSuite(TestM1ProjectContents.class);
-		suite.addTestSuite(TestM0ProjectBasics.class);
-		suite.addTestSuite(TestM0ProjectContents.class);
-		suite.addTestSuite(TestAdapters.class);
-
-		// Builder tests
-		suite.addTestSuite(TestBasicM1ProjectAuditor.class);
-		suite.addTestSuite(TestBasicModelProjectAuditor.class);
-
-		// Facet Tests
-		suite.addTestSuite(BasicFacetTest.class);
-		suite.addTestSuite(TestFacetResolution.class);
-		suite.addTestSuite(TestFacetWithAnnotations.class);
-
-		// Metamodel migration test
-//		suite.addTestSuite(TestArtifactRefactor.class);
-//		suite.addTestSuite(TestFieldMigration.class);
-//		suite.addTestSuite(TestManagedEntityMigration.class);
-//		suite.addTestSuite(TestModelManager.class);
-
-		// Generation Tests
-		suite.addTestSuite(TestProjectGenerationBasics.class);
-		suite.addTestSuite(TestM1Generation.class);
-		suite.addTestSuite(TestM0DeployUndeploy.class);
-
-		// Model operations
-		// suite.addTestSuite(TestModelManager.class); // None of these are
-		// implemented yet!
-		suite.addTestSuite(TestArtifacts.class);
-		suite.addTestSuite(TestFields.class);
-		suite.addTestSuite(TestLiterals.class);
-		suite.addTestSuite(TestMethods.class);
-		suite.addTestSuite(TestTigerstripeWorkspaceNotifications.class);
-
-		// Profiles
-		suite.addTestSuite(TestProfileBasics.class);
-		
-		// Annotations
-		suite.addTestSuite(TestAnnotationCreationAPI.class);
+//		// Startup Tests
+//		suite.addTestSuite(TestStartup.class);
+//
+//		suite.addTestSuite(TestTigerstripeCore.class);
+//
+//		// Basic project operations
+//		suite.addTestSuite(TestProjectManagement.class);
+//		suite.addTestSuite(TestModelProjectLifecycle.class);
+//		suite.addTestSuite(TestM1ProjectBasics.class);
+//		suite.addTestSuite(TestM1ProjectContents.class);
+//		suite.addTestSuite(TestM0ProjectBasics.class);
+//		suite.addTestSuite(TestM0ProjectContents.class);
+//		suite.addTestSuite(TestAdapters.class);
+//
+//		// Builder tests
+//		suite.addTestSuite(TestBasicM1ProjectAuditor.class);
+//		suite.addTestSuite(TestBasicModelProjectAuditor.class);
+//
+//		// Facet Tests
+//		suite.addTestSuite(BasicFacetTest.class);
+//		suite.addTestSuite(TestFacetResolution.class);
+//		suite.addTestSuite(TestFacetWithAnnotations.class);
+//
+//		// Metamodel migration test
+////		suite.addTestSuite(TestArtifactRefactor.class);
+////		suite.addTestSuite(TestFieldMigration.class);
+////		suite.addTestSuite(TestManagedEntityMigration.class);
+////		suite.addTestSuite(TestModelManager.class);
+//
+//		// Generation Tests
+//		suite.addTestSuite(TestProjectGenerationBasics.class);
+//		suite.addTestSuite(TestM1Generation.class);
+//		suite.addTestSuite(TestM0DeployUndeploy.class);
+//
+//		// Model operations
+//		// suite.addTestSuite(TestModelManager.class); // None of these are
+//		// implemented yet!
+//		suite.addTestSuite(TestArtifacts.class);
+//		suite.addTestSuite(TestFields.class);
+//		suite.addTestSuite(TestLiterals.class);
+//		suite.addTestSuite(TestMethods.class);
+//		suite.addTestSuite(TestTigerstripeWorkspaceNotifications.class);
+//
+//		// Profiles
+//		suite.addTestSuite(TestProfileBasics.class);
+//		
+//		// Annotations
+//		suite.addTestSuite(TestAnnotationCreationAPI.class);
 		
 		// $JUnit-END$
 		return suite;
