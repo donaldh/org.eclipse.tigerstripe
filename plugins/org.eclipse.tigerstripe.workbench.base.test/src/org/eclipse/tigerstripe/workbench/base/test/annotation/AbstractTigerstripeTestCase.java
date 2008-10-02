537ff3984cd96b080c9727e61df3b41ff8511808
jworrell <jworrell>
2008-10-02 09:46:55 +0000

    Bug 249454 Added helper methods for access to artufacts and to clean up annotations

--- plugins/org.eclipse.tigerstripe.workbench.base.test/src/org/eclipse/tigerstripe/workbench/base/test/annotation/AbstractTigerstripeTestCase.java
+++ plugins/org.eclipse.tigerstripe.workbench.base.test/src/org/eclipse/tigerstripe/workbench/base/test/annotation/AbstractTigerstripeTestCase.java
@@ -12,21 +12,26 @@
 package org.eclipse.tigerstripe.workbench.base.test.annotation;
 
 import java.util.Collection;
+import java.util.Iterator;
 import java.util.LinkedList;
+import java.util.List;
 
 import junit.framework.TestCase;
 
-import org.eclipse.core.resources.IProject;
-import org.eclipse.core.resources.ResourcesPlugin;
-import org.eclipse.core.runtime.CoreException;
+//import org.eclipse.core.resources.IProject;
+//import org.eclipse.core.resources.ResourcesPlugin;
+//import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.NullProgressMonitor;
+import org.eclipse.tigerstripe.annotation.internal.core.AnnotationManager;
 import org.eclipse.tigerstripe.workbench.TigerstripeCore;
 import org.eclipse.tigerstripe.workbench.TigerstripeException;
 import org.eclipse.tigerstripe.workbench.model.deprecated_.IAbstractArtifact;
 import org.eclipse.tigerstripe.workbench.model.deprecated_.IArtifactManagerSession;
+import org.eclipse.tigerstripe.workbench.model.deprecated_.IModelComponent;
 import org.eclipse.tigerstripe.workbench.project.IAbstractTigerstripeProject;
 import org.eclipse.tigerstripe.workbench.project.IProjectDetails;
 import org.eclipse.tigerstripe.workbench.project.ITigerstripeModelProject;
+import org.eclipse.tigerstripe.workbench.queries.IQueryArtifactsByType;
 
 /**
  * @author Yuri Strot
@@ -112,5 +117,47 @@ public abstract class AbstractTigerstripeTestCase extends TestCase {
 			}
 		}
 		return artifacts;
+	}
+	
+	protected Collection<IAbstractArtifact> getAllArtifacts(IAbstractTigerstripeProject aProject)
+		throws TigerstripeException
+	{
+		ITigerstripeModelProject project = (ITigerstripeModelProject) aProject;
+		IArtifactManagerSession mgrSession = project.getArtifactManagerSession();
+
+		Collection<String> supportedArtifacts = mgrSession.getSupportedArtifacts();
+		Collection<IAbstractArtifact> artifacts = new LinkedList<IAbstractArtifact>();
+		for (String supportedArtifact : supportedArtifacts) {
+			IQueryArtifactsByType query = (IQueryArtifactsByType)mgrSession.makeQuery(IQueryArtifactsByType.class.getName());
+			query.setIncludeDependencies(true);
+			query.setArtifactType(supportedArtifact);
+
+			Collection<IAbstractArtifact> entities = mgrSession.queryArtifact(query);
+//			System.out.println(supportedArtifact+": "+entities);
+			if(entities != null && !entities.isEmpty()) artifacts.addAll(entities);
+		}
+		return artifacts;
+	}
+	
+	protected void cleanUpAnnotations(ITigerstripeModelProject aProject) throws TigerstripeException {
+		List<Object> annotationTargets = new LinkedList<Object>();
+		annotationTargets.add(aProject);
+		Collection<IAbstractArtifact> artifacts = getAllArtifacts(aProject);
+		annotationTargets.addAll(artifacts);
+		for(IAbstractArtifact aa : artifacts)
+		{
+			annotationTargets.addAll(removeNulls(aa.getContainedModelComponents()));
+		}
+		for(Object object : annotationTargets)
+		{
+			AnnotationManager.getInstance().removeAnnotations(object);
+		}
+	}
+	
+	<T> Collection<T> removeNulls(Collection<T> in)
+	{
+		for(Iterator<T> i = in.iterator(); i.hasNext(); )
+			if(i.next() == null) i.remove();
+		return in;
 	}
 }
