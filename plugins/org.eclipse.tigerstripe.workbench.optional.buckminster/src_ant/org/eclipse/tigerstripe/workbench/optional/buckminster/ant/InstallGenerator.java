69793783b575d6bb717820e6cc36b3ca80366f9d
jstrawn <jstrawn>
2008-10-29 19:25:44 +0000

    bug 252602 initial implementation of task (simple hello world, atm)

---
+++ plugins/org.eclipse.tigerstripe.workbench.optional.buckminster/src_ant/org/eclipse/tigerstripe/workbench/optional/buckminster/ant/InstallGenerator.java
@@ -0,0 +1,13 @@
+package org.eclipse.tigerstripe.workbench.optional.buckminster.ant;
+
+import org.apache.tools.ant.BuildException;
+import org.eclipse.ant.core.Task;
+
+public class InstallGenerator extends Task {
+
+	public void execute() throws BuildException {
+		
+		
+	}
+
+}
