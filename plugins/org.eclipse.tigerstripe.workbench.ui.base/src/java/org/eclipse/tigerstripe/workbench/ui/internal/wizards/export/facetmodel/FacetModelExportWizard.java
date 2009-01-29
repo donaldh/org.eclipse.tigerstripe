74663844de743c1f915c37649d3812be381c9645
jstrawn <jstrawn>
2009-01-29 01:12:52 +0000

    ASSIGNED - bug 262652: Export facet content to project https://bugs.eclipse.org/bugs/show_bug.cgi?id=262652 Add new package for facet scoped model export, skeleton class, and update plugin.xml

---
+++ plugins/org.eclipse.tigerstripe.workbench.ui.base/src/java/org/eclipse/tigerstripe/workbench/ui/internal/wizards/export/facetmodel/FacetModelExportWizard.java
@@ -0,0 +1,25 @@
+package org.eclipse.tigerstripe.workbench.ui.internal.wizards.export.facetmodel;
+
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.jface.wizard.Wizard;
+import org.eclipse.ui.IExportWizard;
+import org.eclipse.ui.IWorkbench;
+
+public class FacetModelExportWizard extends Wizard implements IExportWizard {
+
+	public FacetModelExportWizard() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public boolean performFinish() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public void init(IWorkbench workbench, IStructuredSelection selection) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
