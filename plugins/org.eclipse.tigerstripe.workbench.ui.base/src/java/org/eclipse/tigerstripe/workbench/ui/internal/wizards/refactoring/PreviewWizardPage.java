a8829ff955d108b9c969e790f89772fc527c3f6c
jstrawn <jstrawn>
2009-03-31 02:43:23 +0000

    Bugzilla 260907 (GUI) - Refactoring - preview label provider work

--- plugins/org.eclipse.tigerstripe.workbench.ui.base/src/java/org/eclipse/tigerstripe/workbench/ui/internal/wizards/refactoring/PreviewWizardPage.java
+++ plugins/org.eclipse.tigerstripe.workbench.ui.base/src/java/org/eclipse/tigerstripe/workbench/ui/internal/wizards/refactoring/PreviewWizardPage.java
@@ -12,7 +12,6 @@
 package org.eclipse.tigerstripe.workbench.ui.internal.wizards.refactoring;
 
 import org.eclipse.jface.viewers.ArrayContentProvider;
-import org.eclipse.jface.viewers.LabelProvider;
 import org.eclipse.jface.viewers.TableViewer;
 import org.eclipse.jface.viewers.ViewerSorter;
 import org.eclipse.jface.wizard.WizardPage;
@@ -23,33 +22,84 @@ import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Table;
-import org.eclipse.tigerstripe.workbench.IModelChangeDelta;
 import org.eclipse.tigerstripe.workbench.TigerstripeException;
+import org.eclipse.tigerstripe.workbench.internal.api.model.artifacts.updater.request.IArtifactFQRenameRequest;
+import org.eclipse.tigerstripe.workbench.internal.api.model.artifacts.updater.request.IArtifactSetFeatureRequest;
+import org.eclipse.tigerstripe.workbench.internal.core.model.AbstractArtifact;
+import org.eclipse.tigerstripe.workbench.internal.core.model.ModelChangeDelta;
 import org.eclipse.tigerstripe.workbench.ui.EclipsePlugin;
+import org.eclipse.tigerstripe.workbench.ui.internal.views.explorerview.AbstractArtifactLabelProvider;
 
 public class PreviewWizardPage extends WizardPage {
 
-	private class ModelChangeDeltaLabelProvider extends LabelProvider {
+	private class ModelChangeDeltaLabelProvider extends AbstractArtifactLabelProvider {
 
 		@Override
 		public Image getImage(Object element) {
 
-			return null;
+			if (element instanceof ModelChangeDelta) {
+				ModelChangeDelta delta = (ModelChangeDelta) element;
+				return super.getImage(delta.getComponent());
+			} else {
+				return null;
+			}
+
 		}
 
 		@Override
 		public String getText(Object element) {
 
-			if (element instanceof IModelChangeDelta) {
+			if (element instanceof ModelChangeDelta) {
 
-				IModelChangeDelta delta = (IModelChangeDelta) element;
-				return delta.toString();
-				
-			} else {
-				return null;
+				ModelChangeDelta delta = (ModelChangeDelta) element;
+				if (delta.getComponent() instanceof AbstractArtifact) {
+
+					StringBuffer sb = new StringBuffer();
+					if (delta.getFeature().equals(IArtifactFQRenameRequest.FQN_FEATURE)) {
+						sb.append("Move/Rename ");
+						sb.append(delta.getOldValue());
+						sb.append(" to ");
+						sb.append(delta.getNewValue());
+						return sb.toString();
+					} else if (delta.getFeature().equals(IArtifactSetFeatureRequest.EXTENDS_FEATURE)) {
+						AbstractArtifact artifact = (AbstractArtifact) delta.getComponent();
+						sb.append(artifact.getFullyQualifiedName() + " ");
+						sb.append("generalization ");
+						sb.append(delta.getOldValue());
+						sb.append(" updated to ");
+						sb.append(delta.getNewValue());
+					} else if ((delta.getFeature().equals(IArtifactSetFeatureRequest.ZEND) || (delta.getFeature()
+							.equals(IArtifactSetFeatureRequest.AEND)))) {
+						AbstractArtifact artifact = (AbstractArtifact) delta.getComponent();
+						sb.append(artifact.getFullyQualifiedName() + " ");
+						sb.append(delta.getFeature() + " ");
+						sb.append("moved/renamed to ");
+						sb.append(delta.getNewValue());
+					} else {
+						sb.append(delta.toString());
+					}
+
+					return sb.toString();
+
+				} else {
+					return "NOT AN ARTIFACT!";
+				}
+
 			}
+			return null;
 		}
 
+		private String getAssociationLabel(ModelChangeDelta delta) {
+
+			AbstractArtifact artifact = (AbstractArtifact) delta.getComponent();
+			StringBuffer sb = new StringBuffer();
+			sb.append(artifact.getFullyQualifiedName() + " ");
+			sb.append(delta.getFeature() + " ");
+			sb.append("moved/renamed to ");
+			sb.append(delta.getNewValue());
+
+			return sb.toString();
+		}
 	}
 
 	public static final String PAGE_NAME = "PreviewPage";
