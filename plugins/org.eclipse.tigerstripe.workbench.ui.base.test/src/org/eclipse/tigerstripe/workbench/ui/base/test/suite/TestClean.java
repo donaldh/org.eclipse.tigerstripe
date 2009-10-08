5069b0468afbd85b3a71388c1dc908e66a1800c6
edillon <edillon>
2009-10-08 17:20:55 +0000

    Updating wait time for test on UI automation

--- plugins/org.eclipse.tigerstripe.workbench.ui.base.test/src/org/eclipse/tigerstripe/workbench/ui/base/test/suite/TestClean.java
+++ plugins/org.eclipse.tigerstripe.workbench.ui.base.test/src/org/eclipse/tigerstripe/workbench/ui/base/test/suite/TestClean.java
@@ -16,14 +16,14 @@ public class TestClean extends UITestCaseSWT {
 	public void testTestClean() throws Exception {
 		IUIContext ui = getUI();
 		ui.click(new MenuItemLocator("Tigerstripe/Plugins..."));
-		ui.wait(new ShellShowingCondition("Deployed Tigerstripe Plugins"));
+		ui.wait(new ShellShowingCondition("Deployed Tigerstripe Plugins"), 1*60*1000);
 		ui.click(new TableItemLocator("Drop()"));
 		ui.contextClick(new TableItemLocator("Drop()"), "Un-deploy");
-		ui.wait(new ShellShowingCondition("Un-Deploy Tigerstripe Plugin?"));
+		ui.wait(new ShellShowingCondition("Un-Deploy Tigerstripe Plugin?"), 1*60*1000);
 		ui.click(new ButtonLocator("OK"));
-		ui.wait(new ShellDisposedCondition("Un-Deploy Tigerstripe Plugin?"));
+		ui.wait(new ShellDisposedCondition("Un-Deploy Tigerstripe Plugin?"), 1*60*1000);
 		ui.click(new ButtonLocator("OK"));
-		ui.wait(new ShellDisposedCondition("Deployed Tigerstripe Plugins"));
+		ui.wait(new ShellDisposedCondition("Deployed Tigerstripe Plugins"), 1*60*1000);
 	}
 
 }
