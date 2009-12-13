5ff5b7c18a94130c0e3f5fac96a1327fec2eae88
edillon <edillon>
2009-12-13 14:54:40 +0000

    Fixed path to pre-build.

--- pre-build.sh
+++ pre-build.sh
@@ -6,6 +6,10 @@
 DATE=`date`
 ANT=/auto/tigerstripe/local/ant/bin/ant
 
+# Changes are only to be made to the target hierarchy (or else they're not seen
+# by Tycho since Tycho builds that !)
+cd target
+
 # Run javadoc in core plugin so it gets packaged up as part of the documentation
 echo "Running Javadoc in org.eclipse.tigerstripe.workbench.base"
 cd plugins/org.eclipse.tigerstripe.workbench.base
