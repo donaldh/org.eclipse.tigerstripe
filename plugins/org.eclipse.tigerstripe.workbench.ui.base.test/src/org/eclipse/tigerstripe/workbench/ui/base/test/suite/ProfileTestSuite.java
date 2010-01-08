4b985213076431ed6432077978fd55a425dc23be
rcraddock <rcraddock>
2010-01-08 14:59:11 +0000

    Check to view the streotypes i teh explorerer

--- plugins/org.eclipse.tigerstripe.workbench.ui.base.test/src/org/eclipse/tigerstripe/workbench/ui/base/test/suite/ProfileTestSuite.java
+++ plugins/org.eclipse.tigerstripe.workbench.ui.base.test/src/org/eclipse/tigerstripe/workbench/ui/base/test/suite/ProfileTestSuite.java
@@ -11,6 +11,7 @@ import org.eclipse.tigerstripe.workbench.ui.base.test.generator.NewPluginProject
 import org.eclipse.tigerstripe.workbench.ui.base.test.generator.SaveAndDeployPlugin;
 import org.eclipse.tigerstripe.workbench.ui.base.test.profile.AddStereos;
 import org.eclipse.tigerstripe.workbench.ui.base.test.profile.CreateProfile;
+import org.eclipse.tigerstripe.workbench.ui.base.test.profile.Explorer;
 import org.eclipse.tigerstripe.workbench.ui.base.test.profile.SaveAndDeployProfile;
 import org.eclipse.tigerstripe.workbench.ui.base.test.profile.TestActiveProfile;
 import org.eclipse.tigerstripe.workbench.ui.base.test.profile.UpdateProfileArtifacts;
@@ -46,6 +47,8 @@ public class ProfileTestSuite extends TestCase
         suite.addTestSuite(CreateStereotypes.class);
 		// Add one to the relevant object
         suite.addTestSuite(AddStereos.class);
+        
+        suite.addTestSuite(Explorer.class);
 			// Check it appears in the correct place
 			// check it *doesn't* appear in the wrong place?
 		
