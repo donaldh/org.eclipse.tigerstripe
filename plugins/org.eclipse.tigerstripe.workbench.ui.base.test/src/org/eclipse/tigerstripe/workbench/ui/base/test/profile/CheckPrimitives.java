0411a5866196a27c7f58aba8f11b122b1bf3fcb7
rcraddock <rcraddock>
2010-01-15 12:45:24 +0000

    Fixed  a primitive type test

--- plugins/org.eclipse.tigerstripe.workbench.ui.base.test/src/org/eclipse/tigerstripe/workbench/ui/base/test/profile/CheckPrimitives.java
+++ plugins/org.eclipse.tigerstripe.workbench.ui.base.test/src/org/eclipse/tigerstripe/workbench/ui/base/test/profile/CheckPrimitives.java
@@ -1,26 +1,11 @@
 package org.eclipse.tigerstripe.workbench.ui.base.test.profile;
 
-import org.eclipse.swt.widgets.Label;
-import org.eclipse.swt.widgets.Tree;
 import org.eclipse.tigerstripe.workbench.ui.base.test.project.NewArtifacts;
 import org.eclipse.tigerstripe.workbench.ui.base.test.project.ProjectRecord;
 import org.eclipse.tigerstripe.workbench.ui.base.test.suite.TestingConstants;
-import org.eclipse.tigerstripe.workbench.ui.base.test.utils.ArtifactUtils;
-import org.eclipse.tigerstripe.workbench.ui.base.test.utils.GuiUtils;
 
 import com.windowtester.runtime.IUIContext;
 import com.windowtester.runtime.swt.UITestCaseSWT;
-import com.windowtester.runtime.swt.condition.shell.ShellDisposedCondition;
-import com.windowtester.runtime.swt.condition.shell.ShellShowingCondition;
-import com.windowtester.runtime.swt.locator.ButtonLocator;
-import com.windowtester.runtime.swt.locator.CTabItemLocator;
-import com.windowtester.runtime.swt.locator.NamedWidgetLocator;
-import com.windowtester.runtime.swt.locator.SWTWidgetLocator;
-import com.windowtester.runtime.swt.locator.TableItemLocator;
-import com.windowtester.runtime.swt.locator.TreeItemLocator;
-import com.windowtester.runtime.swt.locator.eclipse.ContributedToolItemLocator;
-import com.windowtester.runtime.swt.locator.eclipse.ViewLocator;
-import com.windowtester.runtime.swt.locator.eclipse.WorkbenchLocator;
 
 public class CheckPrimitives extends UITestCaseSWT {
 
