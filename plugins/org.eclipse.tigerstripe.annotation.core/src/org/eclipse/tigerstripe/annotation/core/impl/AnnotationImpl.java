ef0ee909ad831b2d1bbb3234b298c361d82dd284
ystrot <ystrot>
2010-05-17 06:29:32 +0000

    Removed uri checking from equals.

--- plugins/org.eclipse.tigerstripe.annotation.core/src/org/eclipse/tigerstripe/annotation/core/impl/AnnotationImpl.java
+++ plugins/org.eclipse.tigerstripe.annotation.core/src/org/eclipse/tigerstripe/annotation/core/impl/AnnotationImpl.java
@@ -13,7 +13,7 @@
  *     
  * </copyright>
  *
- * $Id: AnnotationImpl.java,v 1.7 2010/04/27 06:57:21 ystrot Exp $
+ * $Id: AnnotationImpl.java,v 1.8 2010/05/17 06:29:32 ystrot Exp $
  */
 package org.eclipse.tigerstripe.annotation.core.impl;
 
@@ -340,8 +340,7 @@ public class AnnotationImpl extends EObjectImpl implements Annotation {
 			Annotation ann = (Annotation) obj;
 			if (ann == null)
 				return false;
-			return equals(ann.getId(), getId())
-					&& equals(ann.getUri(), getUri());
+			return equals(ann.getId(), getId());
 		}
 		return false;
 	}
