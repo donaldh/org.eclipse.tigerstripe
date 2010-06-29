4a866a434541b6ee3c89d4e1146a219db00d959d
ystrot <ystrot>
2010-06-29 10:46:39 +0000

    Return error code if validation failed.

--- plugins/org.eclipse.tigerstripe.workbench.headless/src/org/eclipse/tigerstripe/workbench/headless/Tigerstripe.java
+++ plugins/org.eclipse.tigerstripe.workbench.headless/src/org/eclipse/tigerstripe/workbench/headless/Tigerstripe.java
@@ -12,17 +12,16 @@ package org.eclipse.tigerstripe.workbench.headless;
 
 import java.io.File;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.IResource;
 import org.eclipse.core.resources.IWorkspace;
 import org.eclipse.core.resources.IWorkspaceRunnable;
-import org.eclipse.core.resources.IncrementalProjectBuilder;
 import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.IStatus;
 import org.eclipse.equinox.app.IApplication;
 import org.eclipse.equinox.app.IApplicationContext;
 import org.eclipse.tigerstripe.workbench.TigerstripeCore;
@@ -39,14 +38,13 @@ public class Tigerstripe implements IApplication {
 	private static final String DELIMITER = "=";
 
 	private static final String GENERATION_PROJECT_ARG = "GENERATION_PROJECT";
-	
+
 	private static final String IMPORT_PROJECT_ARG = "PROJECT_IMPORT";
 
 	private List<String> projects;
-	
+
 	private String generationProject;
 
-	@SuppressWarnings("unchecked")
 	public Object start(IApplicationContext context) throws Exception {
 
 		long start = System.currentTimeMillis();
@@ -56,16 +54,17 @@ public class Tigerstripe implements IApplication {
 			printProfile();
 			initializeWorkspace();
 			generateTigerstripeOutput();
-		} catch (TigerstripeException e) {
+		} catch (Exception e) {
 			e.printStackTrace();
-			return new Integer(1);
+			return 1;
 		}
 		long finish = System.currentTimeMillis();
-		System.out.println("Generation complete. Took "+(finish- start)+" milliseconds.");
+		System.out.println("Generation complete. Took " + (finish - start)
+				+ " milliseconds.");
 		return EXIT_OK;
 	}
 
-	public static  void printTigerstipeVersionInfo() {
+	public static void printTigerstipeVersionInfo() {
 		System.out.println(TigerstripeCore.getRuntimeDetails()
 				.getBaseBundleValue(Constants.BUNDLE_NAME)
 				+ " (v"
@@ -73,60 +72,70 @@ public class Tigerstripe implements IApplication {
 						Constants.BUNDLE_VERSION) + ")");
 	}
 
-	private void setPluginParams(IApplicationContext context) throws TigerstripeException {
+	private void setPluginParams(IApplicationContext context)
+			throws TigerstripeException {
 		projects = new ArrayList<String>();
 		String[] split = new String[2];
-		String[] cmdLineArgs = (String[]) context.getArguments().get(IApplicationContext.APPLICATION_ARGS);
+		String[] cmdLineArgs = (String[]) context.getArguments().get(
+				IApplicationContext.APPLICATION_ARGS);
 		for (String arg : cmdLineArgs) {
 			split = arg.split(DELIMITER);
-			if(split[0].equals(IMPORT_PROJECT_ARG)) {
+			if (split[0].equals(IMPORT_PROJECT_ARG)) {
 				projects.add(split[1]);
 			}
-			if(split[0].equals(GENERATION_PROJECT_ARG)) {
+			if (split[0].equals(GENERATION_PROJECT_ARG)) {
 				generationProject = split[1];
 			}
 		}
 		if (generationProject == null) {
-			throw new TigerstripeException("Must have the generation project defined.");
+			throw new TigerstripeException(
+					"Must have the generation project defined.");
 		}
 	}
 
 	private void initializeWorkspace() throws TigerstripeException {
 		importProjectsToWorkspace(projects);
-		for (String project : projects){
+		for (String project : projects) {
 			System.out.println("Imported " + project + " into workspace.");
 		}
 		System.out.println("Generation project: " + generationProject);
 	}
-	
+
 	private void printProfile() {
-		IWorkbenchProfileSession profileSession = TigerstripeCore.getWorkbenchProfileSession();
+		IWorkbenchProfileSession profileSession = TigerstripeCore
+				.getWorkbenchProfileSession();
 		System.out.println("Active Profile: "
 				+ profileSession.getActiveProfile().getName() + " "
 				+ profileSession.getActiveProfile().getVersion());
 	}
 
-	private boolean importProjectsToWorkspace(final List<String> projects) {
+	private void importProjectsToWorkspace(final List<String> projects)
+			throws TigerstripeException {
 
 		IWorkspaceRunnable op = new IWorkspaceRunnable() {
 
 			public void run(IProgressMonitor monitor) throws CoreException {
 				final IWorkspace workspace = ResourcesPlugin.getWorkspace();
-				for (String projectName: projects) {
-					ProjectRecord projectRecord = new ProjectRecord(new File(projectName + File.separator + ".project"));
-					final IProject project = workspace.getRoot().getProject(projectName);
+				for (String projectName : projects) {
+					ProjectRecord projectRecord = new ProjectRecord(new File(
+							projectName + File.separator + ".project"));
+					final IProject project = workspace.getRoot().getProject(
+							projectName);
 					if (!project.exists()) {
 						try {
 							project.create(projectRecord.description, null);
 							project.open(IResource.BACKGROUND_REFRESH, null);
-//							project.refreshLocal(IResource.DEPTH_INFINITE, null);
+							// project.refreshLocal(IResource.DEPTH_INFINITE,
+							// null);
 						} catch (CoreException e) {
 							throw e;
 						}
 					}
 				}
-				workspace.getRoot().refreshLocal(IResource.DEPTH_INFINITE, null);
-//				workspace.build(IncrementalProjectBuilder.FULL_BUILD, monitor);
+				workspace.getRoot()
+						.refreshLocal(IResource.DEPTH_INFINITE, null);
+				// workspace.build(IncrementalProjectBuilder.FULL_BUILD,
+				// monitor);
 			}
 		};
 
@@ -134,10 +143,9 @@ public class Tigerstripe implements IApplication {
 		try {
 			ResourcesPlugin.getWorkspace().run(op, null);
 		} catch (CoreException e) {
-			e.printStackTrace();
-			return false;
+			throw new TigerstripeException(
+					"Importing projects to workspace is failed.", e);
 		}
-		return true;
 	}
 
 	private void generateTigerstripeOutput() throws TigerstripeException {
@@ -145,16 +153,22 @@ public class Tigerstripe implements IApplication {
 		File projectFile = new File(generationProject);
 		ITigerstripeModelProject project = (ITigerstripeModelProject) TigerstripeCore
 				.findProject(projectFile.toURI());
-		IM1RunConfig config = (IM1RunConfig) RunConfig.newGenerationConfig(project, RunConfig.M1);
-		PluginRunStatus[] status = project.generate(config, null);
-
-		if (status.length != 0) {
-			for (PluginRunStatus pluginRunStatus : status) {
+		IM1RunConfig config = (IM1RunConfig) RunConfig.newGenerationConfig(
+				project, RunConfig.M1);
+		PluginRunStatus[] statuses = project.generate(config, null);
+		boolean failed = false;
+		if (statuses.length != 0) {
+			for (PluginRunStatus pluginRunStatus : statuses) {
 				System.out.println(pluginRunStatus);
+				if (pluginRunStatus.getSeverity() == IStatus.ERROR) {
+					failed = true;
+				}
 			}
 		}
+		if (failed)
+			throw new TigerstripeException("Generation is failed.");
 	}
-	
+
 	public void stop() {
 		System.out.println("Stopping");
 	}
