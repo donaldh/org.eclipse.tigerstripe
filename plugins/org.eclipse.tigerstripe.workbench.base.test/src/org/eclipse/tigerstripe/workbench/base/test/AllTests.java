714e240adff774de4faca5dfb1ee58fe377bb369
jstrawn <jstrawn>
2010-10-12 18:38:02 +0000

    Limiting Tests

--- plugins/org.eclipse.tigerstripe.workbench.base.test/src/org/eclipse/tigerstripe/workbench/base/test/AllTests.java
+++ plugins/org.eclipse.tigerstripe.workbench.base.test/src/org/eclipse/tigerstripe/workbench/base/test/AllTests.java
@@ -47,73 +47,61 @@ import org.eclipse.tigerstripe.workbench.internal.core.model.export.facets.TestF
 public class AllTests {
 
 	public static Test suite() {
-		TestSuite suite = new TestSuite(
-				"Test for org.eclipse.tigerstripe.workbench.base.test");
-		// $JUnit-BEGIN$
 
-		// // Startup Tests
-		suite.addTestSuite(TestStartup.class);
+		TestSuite suite = new TestSuite("org.eclipse.tigerstripe.workbench.base.test");
 
-		suite.addTestSuite(TestTigerstripeCore.class);
+		// Startup Tests
+		//suite.addTestSuite(TestStartup.class);
+		//suite.addTestSuite(TestTigerstripeCore.class);
 
 		// Basic project operations
-		suite.addTestSuite(TestProjectManagement.class);
-		suite.addTestSuite(TestModelProjectLifecycle.class);
-		suite.addTestSuite(TestM1ProjectBasics.class);
-		suite.addTestSuite(TestM1ProjectContents.class);
-		suite.addTestSuite(TestM0ProjectBasics.class);
-		suite.addTestSuite(TestM0ProjectContents.class);
-		suite.addTestSuite(TestAdapters.class);
-		//
-		// // Builder tests
-		suite.addTestSuite(TestBasicM1ProjectAuditor.class);
-		suite.addTestSuite(TestBasicModelProjectAuditor.class);
+		//suite.addTestSuite(TestProjectManagement.class);
+		//suite.addTestSuite(TestModelProjectLifecycle.class);
+		//suite.addTestSuite(TestM1ProjectBasics.class);
+		//suite.addTestSuite(TestM1ProjectContents.class);
+		//suite.addTestSuite(TestM0ProjectBasics.class);
+		//suite.addTestSuite(TestM0ProjectContents.class);
+		//suite.addTestSuite(TestAdapters.class);
+		
+		// Builder tests
+		//suite.addTestSuite(TestBasicM1ProjectAuditor.class);
+		//suite.addTestSuite(TestBasicModelProjectAuditor.class);
 
 		// Facet Tests
 		suite.addTestSuite(BasicFacetTest.class);
-		suite.addTestSuite(TestFacetResolution.class);
-//		suite.addTestSuite(TestFacetWithAnnotations.class);
-
-		// Metamodel migration test
-		// suite.addTestSuite(TestArtifactRefactor.class);
-		// suite.addTestSuite(TestFieldMigration.class);
-		// suite.addTestSuite(TestManagedEntityMigration.class);
-		// suite.addTestSuite(TestModelManager.class);
+		//suite.addTestSuite(TestFacetResolution.class);
 
 		// Generation Tests
-		suite.addTestSuite(TestProjectGenerationBasics.class);
-		suite.addTestSuite(TestM1Generation.class);
-		suite.addTestSuite(TestM0DeployUndeploy.class);
+		//suite.addTestSuite(TestProjectGenerationBasics.class);
+		//suite.addTestSuite(TestM1Generation.class);
+		//suite.addTestSuite(TestM0DeployUndeploy.class);
 
 		// Model operations
-		// suite.addTestSuite(TestModelManager.class); // None of these are
-		// implemented yet!
-		suite.addTestSuite(TestArtifacts.class);
-		suite.addTestSuite(TestFields.class);
-		suite.addTestSuite(TestLiterals.class);
-		suite.addTestSuite(TestMethods.class);
-		suite.addTestSuite(TestTigerstripeWorkspaceNotifications.class);
+		//suite.addTestSuite(TestArtifacts.class);
+		//suite.addTestSuite(TestFields.class);
+		//suite.addTestSuite(TestLiterals.class);
+		//suite.addTestSuite(TestMethods.class);
+		//suite.addTestSuite(TestTigerstripeWorkspaceNotifications.class);
 
 		// Profiles
-		suite.addTestSuite(TestProfileBasics.class);
+		//suite.addTestSuite(TestProfileBasics.class);
 
 		// Annotations
-		suite.addTestSuite(TestAnnotationCreationAPI.class);
+		//suite.addTestSuite(TestAnnotationCreationAPI.class);
 
 		// Patterns
-		suite.addTestSuite(PatternTest.class);
-		suite.addTestSuite(ProjectPatternTest.class);
+		//suite.addTestSuite(PatternTest.class);
+		//suite.addTestSuite(ProjectPatternTest.class);
 
 		// References
-		suite.addTestSuite(TestProjectReferences1.class);
-		suite.addTestSuite(TestModelReferences.class);
+		//suite.addTestSuite(TestProjectReferences1.class);
+		//suite.addTestSuite(TestModelReferences.class);
 
 		// Export
-		suite.addTestSuite(TestExportDiff.class);
-		suite.addTestSuite(TestExportFacetManager.class);
-		suite.addTestSuite(TestFacetExporter.class);
+		//suite.addTestSuite(TestExportDiff.class);
+		//suite.addTestSuite(TestExportFacetManager.class);
+		//suite.addTestSuite(TestFacetExporter.class);
 
-		// $JUnit-END$
 		return suite;
 	}
 }
