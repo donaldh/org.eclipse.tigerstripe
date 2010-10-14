c3e84e6e561484931ee36285062af5faebf94854
nmehregani <nmehregani>
2010-10-14 21:44:59 +0000

    Bug 327368 -  UML2 Export NPE on error  Submitting patch for Dan

--- plugins/org.eclipse.tigerstripe.workbench.optional.UML2Export/src/org/eclipse/tigerstripe/workbench/optional/UML2Export/ui/wizards/UML2ExportWizard.java
+++ plugins/org.eclipse.tigerstripe.workbench.optional.UML2Export/src/org/eclipse/tigerstripe/workbench/optional/UML2Export/ui/wizards/UML2ExportWizard.java
@@ -18,11 +18,13 @@ import java.util.Map;
 
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.Status;
 import org.eclipse.jface.dialogs.IDialogSettings;
 import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.jface.operation.IRunnableWithProgress;
 import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.jface.wizard.Wizard;
+import org.eclipse.tigerstripe.repository.internal.Activator;
 import org.eclipse.tigerstripe.workbench.internal.core.TigerstripeRuntime;
 import org.eclipse.tigerstripe.workbench.internal.core.util.messages.MessageList;
 import org.eclipse.tigerstripe.workbench.optional.UML2Export.ProfileAnnotations2UML2;
@@ -47,145 +49,161 @@ import org.eclipse.uml2.uml.Type;
 
 public class UML2ExportWizard extends Wizard implements INewWizard {
 
-	private UML2ExportWizardPage firstPage;
+    private UML2ExportWizardPage firstPage;
 
-	private IStructuredSelection fSelection;
+    private IStructuredSelection fSelection;
 
-	private PrintWriter out;
-	private MessageList messages;
-	private IDialogSettings wizardSettings;
+    private PrintWriter out;
+    private MessageList messages;
+    private IDialogSettings wizardSettings;
 
-	public IStructuredSelection getSelection() {
-		return this.fSelection;
-	}
+    public IStructuredSelection getSelection() {
+        return this.fSelection;
+    }
 
-	/**
-	 * Constructor for NewProjectWizard.
-	 */
-	public UML2ExportWizard() {
-		super();
-		setNeedsProgressMonitor(true);
+    /**
+     * Constructor for NewProjectWizard.
+     */
+    public UML2ExportWizard() {
+        super();
+        setNeedsProgressMonitor(true);
 
-		setWindowTitle("Export to UML2 ...");
-		messages = new MessageList();
-		
-		IDialogSettings uml2ImportSettings = UML2ExportPlugin.getDefault().getDialogSettings();
-		this.wizardSettings = uml2ImportSettings.getSection("UML2ExportWizard");
-		if (wizardSettings == null){
-			wizardSettings = uml2ImportSettings.addNewSection("UML2ExportWizard");
-		}
-		setDialogSettings(uml2ImportSettings);	
-	}
+        setWindowTitle("Export to UML2 ...");
+        messages = new MessageList();
 
-	/**
-	 * Adding the page to the wizard.
-	 */
+        IDialogSettings uml2ImportSettings = UML2ExportPlugin.getDefault()
+                .getDialogSettings();
+        this.wizardSettings = uml2ImportSettings.getSection("UML2ExportWizard");
+        if (wizardSettings == null) {
+            wizardSettings = uml2ImportSettings
+                    .addNewSection("UML2ExportWizard");
+        }
+        setDialogSettings(uml2ImportSettings);
+    }
 
-	@Override
-	public void addPages() {
-		super.addPages();
+    /**
+     * Adding the page to the wizard.
+     */
 
-		this.firstPage = new UML2ExportWizardPage(wizardSettings);
+    @Override
+    public void addPages() {
+        super.addPages();
 
-		addPage(this.firstPage);
+        this.firstPage = new UML2ExportWizardPage(wizardSettings);
 
-		this.firstPage.init(getSelection());
-	}
+        addPage(this.firstPage);
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see org.eclipse.ui.IWorkbenchWizard#init(org.eclipse.ui.IWorkbench,
-	 *      org.eclipse.jface.viewers.IStructuredSelection)
-	 */
-	public void init(IWorkbench workbench, IStructuredSelection currentSelection) {
-		fSelection = currentSelection;
-	}
+        this.firstPage.init(getSelection());
+    }
 
-	/**
-	 * This method is called when 'Finish' button is pressed in the wizard. We
-	 * will create an operation and run it using wizard as execution context.
-	 */
-	@Override
-	public boolean performFinish() {
+    /*
+     * (non-Javadoc)
+     * 
+     * @see org.eclipse.ui.IWorkbenchWizard#init(org.eclipse.ui.IWorkbench,
+     * org.eclipse.jface.viewers.IStructuredSelection)
+     */
+    public void init(IWorkbench workbench, IStructuredSelection currentSelection) {
+        fSelection = currentSelection;
+    }
 
-		IRunnableWithProgress op = new IRunnableWithProgress() {
-			public void run(IProgressMonitor monitor)
-					throws InvocationTargetException {
-				try {
-					doExport(monitor);
-				} catch (CoreException e) {
-					throw new InvocationTargetException(e);
-				} finally {
-					monitor.done();
-				}
-			}
-		};
-		try {
-			getContainer().run(false, false, op);
-		} catch (InterruptedException e) {
-			return false;
-		} catch (InvocationTargetException e) {
-			Throwable realException = e.getTargetException();
-			MessageDialog.openError(getShell(), "Error", realException
-					.getMessage());
-			return false;
-		} finally {
-			wizardSettings.put("TSProject",firstPage.getTigerstripeName());
-			wizardSettings.put("ExportDir",firstPage.getExportDir().toString());	
-		}
-		return true;
-	}
+    /**
+     * This method is called when 'Finish' button is pressed in the wizard. We
+     * will create an operation and run it using wizard as execution context.
+     */
+    @Override
+    public boolean performFinish() {
 
-	public boolean doExport(IProgressMonitor monitor) throws CoreException {
+        IRunnableWithProgress op = new IRunnableWithProgress() {
+            public void run(IProgressMonitor monitor)
+                    throws InvocationTargetException {
+                try {
+                    doExport(monitor);
+                } catch (Exception e) {
+                    throw new InvocationTargetException(e);
+                } finally {
+                    monitor.done();
+                }
+            }
+        };
+        try {
+            getContainer().run(false, false, op);
+        } catch (InterruptedException e) {
+            return false;
+        } catch (InvocationTargetException e) {
+            MessageDialog.openError(getShell(), "Error", e.getTargetException()
+                    .getMessage());
+            return false;
+        } finally {
+            wizardSettings.put("TSProject", firstPage.getTigerstripeName());
+            wizardSettings
+                    .put("ExportDir", firstPage.getExportDir().toString());
+        }
+        return true;
+    }
 
-		try {
-			// Restart the list in case a new XML has been created.
-			messages = new MessageList();
+    public boolean doExport(IProgressMonitor monitor) throws CoreException {
+        try {
+            // Check if directory exists, ask to create it if it does not
+            if (!this.firstPage.getExportDir().exists()) {
+                boolean createDirectory = MessageDialog
+                        .openQuestion(
+                                getShell(),
+                                "Export Directory Does Not Exist",
+                                "The export directory specified \""
+                                        + this.firstPage.getExportDir()
+                                                .getPath()
+                                        + "\" does not exist. Would you like it to be created?");
+                if (createDirectory) {
+                    (new File(firstPage.getExportDir().getPath())).mkdir();
+                }
+            }
+            // Restart the list in case a new XML has been created.
+            messages = new MessageList();
 
-			File logFile = new File(this.firstPage.getExportDir()
-					+ File.separator + "UML2Export.log");
-			out = new PrintWriter(new FileOutputStream(logFile, false));
+            File logFile = new File(this.firstPage.getExportDir()
+                    + File.separator + "UML2Export.log");
+            out = new PrintWriter(new FileOutputStream(logFile, false));
 
-			// Make profile and Modelelements stuff from the current profile
-			// info.
-			ProfileTypes2UML2 p2uml = new ProfileTypes2UML2();
-			Map<String, Type> primitiveTypeMap = p2uml.loadTSProfileTypestoUML(
-					this.firstPage.getExportDir(), this.firstPage
-							.getTigerstripeName(), out, messages, monitor);
-			Model typesModel = p2uml.getModel();
+            // Make profile and Modelelements stuff from the current profile
+            // info.
+            ProfileTypes2UML2 p2uml = new ProfileTypes2UML2();
+            Map<String, Type> primitiveTypeMap = p2uml
+                    .loadTSProfileTypestoUML(this.firstPage.getExportDir(),
+                            this.firstPage.getTigerstripeName(), out, messages,
+                            monitor);
+            Model typesModel = p2uml.getModel();
 
-			ProfileAnnotations2UML2 a2uml = new ProfileAnnotations2UML2(out);
-			Profile profile = a2uml.loadTSProfileAnnotationstoUML(
-					this.firstPage.getExportDir(), this.firstPage
-							.getTigerstripeName(),  messages, monitor,
-					primitiveTypeMap, typesModel);
+            ProfileAnnotations2UML2 a2uml = new ProfileAnnotations2UML2(out);
+            Profile profile = a2uml.loadTSProfileAnnotationstoUML(
+                    this.firstPage.getExportDir(),
+                    this.firstPage.getTigerstripeName(), messages, monitor,
+                    primitiveTypeMap, typesModel);
 
-			TS2UML ts2uml = new TS2UML(out, messages, monitor);
-			ts2uml.loadTigerstripeToUML(this.firstPage.getExportDir(),
-					this.firstPage.getTigerstripeName(), this.firstPage
-							.getTigerstripeName(), 
-					primitiveTypeMap, profile, typesModel);
+            TS2UML ts2uml = new TS2UML(out, messages, monitor);
+            ts2uml.loadTigerstripeToUML(this.firstPage.getExportDir(),
+                    this.firstPage.getTigerstripeName(),
+                    this.firstPage.getTigerstripeName(), primitiveTypeMap,
+                    profile, typesModel);
 
-			out.close();
+            out.close();
 
-			monitor.done();
+            monitor.done();
 
-			if (!messages.isEmpty()) {
-				MessageListDialog msgDialog = new MessageListDialog(this
-						.getShell(), messages, "UML2 Export Log");
-				msgDialog.open();
-			}
+            if (!messages.isEmpty()) {
+                MessageListDialog msgDialog = new MessageListDialog(
+                        this.getShell(), messages, "UML2 Export Log");
+                msgDialog.open();
+            }
 
-			return true;
+            return true;
 
-		} catch (Exception e) {
-			TigerstripeRuntime.logErrorMessage("Exception detected", e);
-			e.printStackTrace(out);
-			out.close();
-			monitor.done();
-			return false;
-		}
-	}
-
+        } catch (Exception e) {
+            if (out != null) {
+                TigerstripeRuntime.logErrorMessage("Exception detected", e);
+                e.printStackTrace(out);
+            }
+            throw new CoreException(new Status(Status.ERROR,
+                    Activator.PLUGIN_ID, e.getMessage()));
+        }
+    }
 }
