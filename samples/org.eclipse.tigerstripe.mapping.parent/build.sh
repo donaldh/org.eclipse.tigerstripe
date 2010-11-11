213157cdd44927426678e2432fc4258aa730e59f
jstrawn <jstrawn>
2010-11-11 14:55:52 +0000

    Changes to build.sh

--- samples/org.eclipse.tigerstripe.mapping.parent/build.sh
+++ samples/org.eclipse.tigerstripe.mapping.parent/build.sh
@@ -2,7 +2,8 @@
 
 export MAVEN_OPTS=-Xmx1024m
 
-if[ ! -e $WORKSPACE/repository ]; then
-	echo " ## Creating repository directory in workspace."
-	mkdir -p $WORKSPACE/repository
+if[ ! -e $WORKSPACE/repository ]
+	then
+		echo " ## Creating repository directory in workspace."
+		mkdir -p $WORKSPACE/repository
 fi
