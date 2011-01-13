1d6f828b02ce85e3c9fb1c245a69f12c29390059
ystrot <ystrot>
2011-01-13 06:10:18 +0000

    Fixed bug 333464 - "Add Velocity Template" ignores selection.

--- plugins/org.eclipse.tigerstripe.workbench.ui.base/src/java/org/eclipse/tigerstripe/workbench/ui/internal/wizards/pluggablePlugin/NewVelocityTemplateWizard.java
+++ plugins/org.eclipse.tigerstripe.workbench.ui.base/src/java/org/eclipse/tigerstripe/workbench/ui/internal/wizards/pluggablePlugin/NewVelocityTemplateWizard.java
@@ -3,14 +3,17 @@ package org.eclipse.tigerstripe.workbench.ui.internal.wizards.pluggablePlugin;
 import java.lang.reflect.InvocationTargetException;
 
 import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.core.resources.IContainer;
 import org.eclipse.core.resources.IFile;
-import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.core.runtime.IPath;
 import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.Path;
 import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.jface.operation.IRunnableWithProgress;
 import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.tigerstripe.workbench.ui.EclipsePlugin;
+import org.eclipse.tigerstripe.workbench.ui.internal.wizards.BasicNewResourceWizard;
 import org.eclipse.ui.INewWizard;
 import org.eclipse.ui.IWorkbench;
 import org.eclipse.ui.IWorkbenchPage;
@@ -20,23 +23,24 @@ import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.ide.IDE;
 import org.eclipse.ui.ide.undo.CreateFileOperation;
 import org.eclipse.ui.ide.undo.WorkspaceUndoUtil;
-import org.eclipse.tigerstripe.workbench.ui.internal.wizards.BasicNewResourceWizard;
 
 /**
  * Velocity template wizard (enhancement#324709)
  * 
  * @author Navid Mehregani - Initial Creation
  */
-public class NewVelocityTemplateWizard extends BasicNewResourceWizard implements INewWizard {
+public class NewVelocityTemplateWizard extends BasicNewResourceWizard implements
+		INewWizard {
 
 	private NewVelocityWizardPage fWizardPage = null;
 	private IStructuredSelection fSelection = null;
-	
+
+	@Override
 	public void init(IWorkbench workbench, IStructuredSelection selection) {
 		fSelection = selection;
 		fWizardPage = new NewVelocityWizardPage(fSelection);
 	}
-	
+
 	@Override
 	public void addPages() {
 		addPage(fWizardPage);
@@ -45,67 +49,84 @@ public class NewVelocityTemplateWizard extends BasicNewResourceWizard implements
 	@Override
 	public boolean performFinish() {
 		final String fileName = fWizardPage.getFilename();
-		String projectName = fWizardPage.getProjectName();
-		
-		if ((!isStringValid(fileName)) || (!isStringValid(projectName))) {
+		IPath targetPath = fWizardPage.getTargetContainer();
+
+		if ((!isFileNameValid(fileName)) || !isTargetContainerValid(targetPath)) {
 			// We should never get to this point
-			EclipsePlugin.logErrorMessage("Velocity template wizard: either filename or project name is invalid.");
+			EclipsePlugin
+					.logErrorMessage("Velocity template wizard: either filename or target folder is invalid.");
 			return false;
 		}
-		
-		IProject project = ResourcesPlugin.getWorkspace().getRoot().getProject(projectName);
-		
-		if (project!=null) {
-			final IFile file = project.getFile("templates/" + fileName);
+
+		IContainer targetContainer = ResourcesPlugin.getWorkspace().getRoot()
+				.getFolder(targetPath);
+
+		if (targetContainer != null) {
+			final IFile file = targetContainer.getFile(new Path(fileName));
 			if (!file.exists()) {
-				
 				IRunnableWithProgress runnableWithProgress = new IRunnableWithProgress() {
 
-					public void run(IProgressMonitor monitor) throws InvocationTargetException,	InterruptedException {
-						CreateFileOperation fileOperation = new CreateFileOperation(file, null, null, "Creating file " + fileName);					
+					public void run(IProgressMonitor monitor)
+							throws InvocationTargetException,
+							InterruptedException {
+						CreateFileOperation fileOperation = new CreateFileOperation(
+								file, null, null, "Creating file " + fileName);
 						try {
-							fileOperation.execute(monitor, WorkspaceUndoUtil.getUIInfoAdapter(getShell()));
+							fileOperation.execute(monitor, WorkspaceUndoUtil
+									.getUIInfoAdapter(getShell()));
 						} catch (ExecutionException e) {
-							EclipsePlugin.logErrorMessage("An error occurred while creating the file: " + e.getMessage());
+							EclipsePlugin
+									.logErrorMessage("An error occurred while creating the file: "
+											+ e.getMessage());
 						}
 					}
-					
+
 				};
-				
+
 				try {
 					getContainer().run(true, false, runnableWithProgress);
 				} catch (InvocationTargetException e1) {
-					EclipsePlugin.logErrorMessage("An error occurred while creating the file: " + e1.getMessage());
+					EclipsePlugin
+							.logErrorMessage("An error occurred while creating the file: "
+									+ e1.getMessage());
 					return false;
 				} catch (InterruptedException e1) {
-					EclipsePlugin.logErrorMessage("An error occurred while creating the file: " + e1.getMessage());
+					EclipsePlugin
+							.logErrorMessage("An error occurred while creating the file: "
+									+ e1.getMessage());
 					return false;
 				}
-				
-				
-				IWorkbenchWindow dw = PlatformUI.getWorkbench().getActiveWorkbenchWindow();
-				  
+
+				IWorkbenchWindow dw = PlatformUI.getWorkbench()
+						.getActiveWorkbenchWindow();
+
 				selectAndReveal(file, dw);
 
-		        // Open editor on new file.
-		        try {
-		            if (dw != null) {
-		                IWorkbenchPage page = dw.getActivePage();
-		                if (page != null) {
-		                    IDE.openEditor(page, file, true);
-		                }
-		            }
-		        } catch (PartInitException e) {
-		    		MessageDialog messageDialog = new MessageDialog(PlatformUI.getWorkbench().getDisplay().getActiveShell(), "Error", null, "An error occurred while opening the newly created file in editor", MessageDialog.ERROR, new String[]{"OK"}, 0); 
-		    		messageDialog.open();	
-		        }
+				// Open editor on new file.
+				try {
+					if (dw != null) {
+						IWorkbenchPage page = dw.getActivePage();
+						if (page != null) {
+							IDE.openEditor(page, file, true);
+						}
+					}
+				} catch (PartInitException e) {
+					MessageDialog messageDialog = new MessageDialog(
+							PlatformUI.getWorkbench().getDisplay()
+									.getActiveShell(),
+							"Error",
+							null,
+							"An error occurred while opening the newly created file in editor",
+							MessageDialog.ERROR, new String[] { "OK" }, 0);
+					messageDialog.open();
+				}
 			} else {
-				EclipsePlugin.logErrorMessage("Specified file already exists in this project");
+				EclipsePlugin.logErrorMessage("Specified file already exists");
 				return false;
 			}
-			
+
 		}
-		
+
 		return true;
 	}
 
@@ -113,9 +134,12 @@ public class NewVelocityTemplateWizard extends BasicNewResourceWizard implements
 	public boolean canFinish() {
 		return fWizardPage.canFinish();
 	}
-	
-	private boolean isStringValid(String text) {
-		return ((text!=null) && (text.trim().length()!=0));
+
+	private boolean isFileNameValid(String text) {
+		return ((text != null) && (text.trim().length() != 0));
 	}
 
+	private boolean isTargetContainerValid(IPath targetContainer) {
+		return targetContainer != null && targetContainer.segmentCount() > 0;
+	}
 }
--- plugins/org.eclipse.tigerstripe.workbench.ui.base/src/java/org/eclipse/tigerstripe/workbench/ui/internal/wizards/pluggablePlugin/NewVelocityWizardPage.java
+++ plugins/org.eclipse.tigerstripe.workbench.ui.base/src/java/org/eclipse/tigerstripe/workbench/ui/internal/wizards/pluggablePlugin/NewVelocityWizardPage.java
@@ -1,28 +1,43 @@
 package org.eclipse.tigerstripe.workbench.ui.internal.wizards.pluggablePlugin;
 
 import java.util.ArrayList;
+import java.util.List;
 
+import org.eclipse.core.resources.IContainer;
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IProject;
-import org.eclipse.core.resources.IProjectNature;
 import org.eclipse.core.resources.IResource;
+import org.eclipse.core.resources.IWorkspace;
 import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IPath;
 import org.eclipse.core.runtime.Path;
-import org.eclipse.jdt.core.IJavaProject;
-import org.eclipse.jdt.core.IPackageFragmentRoot;
+import org.eclipse.jface.dialogs.Dialog;
+import org.eclipse.jface.viewers.DoubleClickEvent;
+import org.eclipse.jface.viewers.IDoubleClickListener;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.ISelectionChangedListener;
 import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.jface.viewers.ITreeContentProvider;
+import org.eclipse.jface.viewers.SelectionChangedEvent;
+import org.eclipse.jface.viewers.StructuredSelection;
+import org.eclipse.jface.viewers.TreeViewer;
+import org.eclipse.jface.viewers.Viewer;
+import org.eclipse.jface.viewers.ViewerComparator;
 import org.eclipse.jface.wizard.WizardPage;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.ModifyEvent;
 import org.eclipse.swt.events.ModifyListener;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
-import org.eclipse.swt.widgets.Combo;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Event;
 import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Listener;
 import org.eclipse.swt.widgets.Text;
 import org.eclipse.tigerstripe.workbench.ui.EclipsePlugin;
+import org.eclipse.tigerstripe.workbench.ui.internal.views.explorerview.TigerstripeLabelProvider;
+import org.eclipse.ui.part.DrillDownComposite;
 
 /**
  * Velocity template wizard page (enhancement#324709)
@@ -30,13 +45,13 @@ import org.eclipse.tigerstripe.workbench.ui.EclipsePlugin;
  * @author Navid Mehregani - Initial Creation
  */
 public class NewVelocityWizardPage extends WizardPage {
-	
+
 	private final String GENERATOR_PROJECT_NATURE_ID = "org.eclipse.tigerstripe.workbench.base.tigerstripePluginProject";
-	private Combo fProjectComboBox = null;
-	private Text  fFilenameText = null;
+	private ContainerSelectionGroup targetContainer;
+	private Text fFilenameText = null;
 	private boolean fCanFinish = false;
-	private String fSelectedProjectName = null;
-	
+	private IContainer initialSelection;
+
 	protected NewVelocityWizardPage(IStructuredSelection selection) {
 		super("Create a new Velocity Template file");
 		setTitle("Velocity Template File");
@@ -44,106 +59,79 @@ public class NewVelocityWizardPage extends WizardPage {
 		if (selection != null) {
 			Object obj = selection.getFirstElement();
 			if (obj instanceof IResource) {
-				IProject project = ((IResource)obj).getProject();
-				if (project!=null) 
-					fSelectedProjectName = project.getName();
-			} else if (obj instanceof IPackageFragmentRoot) {
-				IJavaProject project = ((IPackageFragmentRoot)obj).getJavaProject();
-				if (project!=null) {
-					fSelectedProjectName = project.getElementName();
+				if (obj instanceof IContainer) {
+					initialSelection = (IContainer) obj;
+				} else {
+					initialSelection = ((IResource) obj).getParent();
 				}
 			}
 		}
 	}
-
 
 	public void createControl(Composite parent) {
 		Composite composite = new Composite(parent, SWT.NONE);
-		
+
 		GridLayout layout = new GridLayout();
 		layout.numColumns = 2;
 		composite.setLayout(layout);
-		
-		// Generator Project label
-		Label projectLabel = new Label(composite, SWT.NONE);
-		projectLabel.setLayoutData(new GridData(GridData.HORIZONTAL_ALIGN_BEGINNING));
-		projectLabel.setText("&Generator Project: ");
-		
-		fProjectComboBox = new Combo(composite, SWT.READ_ONLY);
-		
-		GridData projectComboBoxGridData = new GridData();
-		projectComboBoxGridData.horizontalAlignment = GridData.FILL;
-		projectComboBoxGridData.grabExcessHorizontalSpace = true;
-		fProjectComboBox.setLayoutData(projectComboBoxGridData);
-		
-		// Template file name label 
+
+		targetContainer = new ContainerSelectionGroup(composite,
+				new Listener() {
+					public void handleEvent(Event event) {
+						validate();
+					}
+				});
+		GridData gridData = new GridData(GridData.FILL_HORIZONTAL
+				| GridData.GRAB_HORIZONTAL);
+		gridData.horizontalSpan = 2;
+		targetContainer.setLayoutData(gridData);
+
+		// Template file name label
 		Label fileNameLabel = new Label(composite, SWT.NONE);
 		fileNameLabel.setText("Filename: ");
-		
+
 		fFilenameText = new Text(composite, SWT.BORDER);
-		fFilenameText.setLayoutData(new GridData(GridData.HORIZONTAL_ALIGN_BEGINNING));
+		fFilenameText.setLayoutData(new GridData(
+				GridData.HORIZONTAL_ALIGN_BEGINNING));
 		fFilenameText.addModifyListener(new ModifyListener() {
 			public void modifyText(ModifyEvent e) {
 				validate();
-			}			
+			}
 		});
-		
+
 		GridData fileNameTextGridData = new GridData();
 		fileNameTextGridData.horizontalAlignment = GridData.FILL;
 		fileNameTextGridData.grabExcessHorizontalSpace = true;
-		fFilenameText.setLayoutData(fileNameTextGridData);	
-		
+		fFilenameText.setLayoutData(fileNameTextGridData);
+
 		initialize();
-		
+
 		setControl(composite);
-	    setVisible(true);
+		setVisible(true);
+		targetContainer.setInitialFocus();
+		if (initialSelection != null) {
+			targetContainer.setSelectedContainer(initialSelection);
+		}
 	}
-	
+
 	public String getFilename() {
 		return fFilenameText.getText();
 	}
-	
-	public String getProjectName() {
-		return fProjectComboBox.getText();
+
+	public IPath getTargetContainer() {
+		IContainer selectedContainer = targetContainer.getSelectedContainer();
+		if (selectedContainer != null) {
+			return selectedContainer.getFullPath();
+		}
+		return null;
 	}
-	
+
 	private void initialize() {
-		boolean selectedProjectIsValid = false;
-		ArrayList<String> projectNames = new ArrayList<String>();
-		IProject[] projects = ResourcesPlugin.getWorkspace().getRoot().getProjects();
-		for (int i=0; i < projects.length; i++) {
-			IProject project = projects[i];
-			if (project!=null) {
-				try {
-					IProjectNature nature = project.getNature(GENERATOR_PROJECT_NATURE_ID);
-					if (nature!=null) {
-						String projectName = project.getName();
-						if (isStringValid(projectName)) {
-							projectNames.add(projectName);
-							if (projectName.equals(fSelectedProjectName))
-								selectedProjectIsValid = true;
-						}
-					}
-				} catch (CoreException e) {
-					continue;
-				}
-			}
-		}
-		
-		if (projectNames.size() > 0) {
-			String[] projectNameArray = projectNames.toArray(new String[projectNames.size()]);
-			fProjectComboBox.setItems(projectNameArray);
-			if (selectedProjectIsValid)
-				fProjectComboBox.setText(fSelectedProjectName);
-			else
-				fProjectComboBox.setText(projectNameArray[0]);
-		}
-		
 		fFilenameText.setText("myTemplateFile.vm");
 	}
-	
+
 	private boolean isStringValid(String text) {
-		return (text!=null && text.length()>0);
+		return (text != null && text.length() > 0);
 	}
 
 	/**
@@ -155,97 +143,289 @@ public class NewVelocityWizardPage extends WizardPage {
 		String fileName = fFilenameText.getText();
 		String fileExtension = null;
 		final String INVALID_CHARACTERS = "~!@#$%^&*()+ ";
-		String selectedProject = fProjectComboBox.getText();
-		
+		IContainer selectedContainer = targetContainer.getSelectedContainer();
+
 		fCanFinish = true;
-		
+
 		// Check selected project
-		if (selectedProject==null || selectedProject.length()==0) {
-			String errorMessage = "Generator project not selected. Velocity template files can only be created for generator projects. Make sure there is at least one defined in the workspace.";
+		if (selectedContainer == null || selectedContainer instanceof IProject) {
+			String errorMessage = "Destination folder isn't selected. Velocity template files"
+					+ " can only be created for generator projects.";
 			setErrorMessage(errorMessage);
-    		setMessage(errorMessage, WizardPage.ERROR);
-    		fCanFinish = false;
-		}		
+			setMessage(errorMessage, WizardPage.ERROR);
+			fCanFinish = false;
+		}
 		// Filename is empty
-		else if (fileName==null || fileName.length()==0) {
+		else if (fileName == null || fileName.length() == 0) {
 			String errorMessage = "Filename cannot be empty";
 			setErrorMessage(errorMessage);
-    		setMessage(errorMessage, WizardPage.ERROR);
-    		fCanFinish = false;
-		}		
+			setMessage(errorMessage, WizardPage.ERROR);
+			fCanFinish = false;
+		}
 		// Filename ends with '.'
 		else if (fileName.endsWith(".")) {
 			setErrorMessage("Filename cannot end with '.'");
-    		setMessage("Filename cannot end with '.'", WizardPage.ERROR);
-    		fCanFinish = false;
-		}		
+			setMessage("Filename cannot end with '.'", WizardPage.ERROR);
+			fCanFinish = false;
+		}
 		// Check file extension, make sure it's 'vm'
 		else if (fileName.contains(".")) {
 			fileExtension = fileName.substring(fileName.indexOf(".") + 1);
 			if (!fileExtension.equalsIgnoreCase("vm")) {
 				setErrorMessage("File extension must be 'vm'");
-	    		setMessage("File extension must be 'vm'", WizardPage.ERROR);
-	    		fCanFinish = false;
+				setMessage("File extension must be 'vm'", WizardPage.ERROR);
+				fCanFinish = false;
 			}
-		} 
-		
-		// Do further checking.  This can't be an 'else if' block, because the file can contain '.', but still be valid.
+		}
+
+		// Do further checking. This can't be an 'else if' block, because the
+		// file can contain '.', but still be valid.
 		if (fCanFinish) {
 			// Does the template file already exist?
-			if (doesFileExist(selectedProject, fileName)) {
-				setErrorMessage("Specified file already exists in the selected project");
-	    		setMessage("Specified file already exists in the selected project", WizardPage.ERROR);
-	    		fCanFinish = false;
+			if (doesFileExist(selectedContainer, fileName)) {
+				setErrorMessage("Specified file already exists");
+				setMessage("Specified file already exists", WizardPage.ERROR);
+				fCanFinish = false;
 			}
 			// Does filename contain invalid characters?
 			else {
-				for (int i=0, n=INVALID_CHARACTERS.length(); i < n; i++) {
-					if (fileName.contains(String.valueOf(INVALID_CHARACTERS.charAt(i)))) {
+				for (int i = 0, n = INVALID_CHARACTERS.length(); i < n; i++) {
+					if (fileName.contains(String.valueOf(INVALID_CHARACTERS
+							.charAt(i)))) {
 						setErrorMessage("Filename is invalid");
-			    		setMessage("Filename is invalid", WizardPage.ERROR);
-			    		fCanFinish = false;
+						setMessage("Filename is invalid", WizardPage.ERROR);
+						fCanFinish = false;
 					}
 				}
 			}
 		}
-		
-		
+
 		if (fCanFinish) {
 			setErrorMessage(null);
 			setMessage(null);
 		}
-		
-		getContainer().updateButtons();		
+
+		getContainer().updateButtons();
 		return fCanFinish;
 	}
-	
+
 	public boolean canFinish() {
 		return fCanFinish;
 	}
-	
-	private boolean doesFileExist(String projectName, String fileName) {
-		
-		if ((!isStringValid(projectName)) || (!isStringValid(fileName))) {
-			EclipsePlugin.logErrorMessage("Velocity template wizard (validation): project name and/or filename is invalid");
+
+	private boolean doesFileExist(IContainer container, String fileName) {
+		if (container == null || (!isStringValid(fileName))) {
+			EclipsePlugin
+					.logErrorMessage("Velocity template wizard (validation): folder and/or filename is invalid");
 			return false;
 		}
-		
-		try {
-			IProject project = ResourcesPlugin.getWorkspace().getRoot().getProject(projectName);
-			if (project!=null) {
-				if ((projectName.equals(project.getName())) && (project.getNature(GENERATOR_PROJECT_NATURE_ID)!=null)) {
-					IFile file = project.getFile(new Path("/templates/" + fileName));
-					if ((file!=null) && (file.exists()))
-						return true;
-				}
-			}
-		} catch(CoreException e) {
-			EclipsePlugin.logErrorMessage("Velocity template wizard (validation): exception - " + e.getMessage());
-		}
-		
+
+		IFile file = container.getFile(new Path(fileName));
+		if ((file != null) && (file.exists()))
+			return true;
+
 		return false;
-		
+
 	}
 
+	private class ContainerSelectionGroup extends Composite {
+		private final Listener listener;
 
+		private IContainer selectedContainer;
+
+		private TreeViewer treeViewer;
+
+		private static final int SIZING_SELECTION_PANE_WIDTH = 320;
+
+		private static final int SIZING_SELECTION_PANE_HEIGHT = 300;
+
+		public ContainerSelectionGroup(Composite parent, Listener listener) {
+			this(parent, listener, null);
+		}
+
+		public ContainerSelectionGroup(Composite parent, Listener listener,
+				String message) {
+			this(parent, listener, message, SIZING_SELECTION_PANE_HEIGHT,
+					SIZING_SELECTION_PANE_WIDTH);
+		}
+
+		public ContainerSelectionGroup(Composite parent, Listener listener,
+				String message, int heightHint, int widthHint) {
+			super(parent, SWT.NONE);
+			this.listener = listener;
+			if (message != null) {
+				createContents(message, heightHint, widthHint);
+			} else {
+				createContents("Select the folder:", heightHint, widthHint);
+			}
+		}
+
+		private void containerSelectionChanged(IContainer container) {
+			selectedContainer = container;
+
+			if (listener != null) {
+				Event changeEvent = new Event();
+				changeEvent.type = SWT.Selection;
+				changeEvent.widget = this;
+				listener.handleEvent(changeEvent);
+			}
+		}
+
+		private void createContents(String message, int heightHint,
+				int widthHint) {
+			GridLayout layout = new GridLayout();
+			layout.marginWidth = 0;
+			setLayout(layout);
+			setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true));
+
+			Label label = new Label(this, SWT.WRAP);
+			label.setText(message);
+			label.setFont(this.getFont());
+
+			createTreeViewer(heightHint);
+			Dialog.applyDialogFont(this);
+		}
+
+		private void createTreeViewer(int heightHint) {
+			DrillDownComposite drillDown = new DrillDownComposite(this,
+					SWT.BORDER);
+			GridData spec = new GridData(SWT.FILL, SWT.FILL, true, true);
+			spec.widthHint = SIZING_SELECTION_PANE_WIDTH;
+			spec.heightHint = heightHint;
+			drillDown.setLayoutData(spec);
+
+			treeViewer = new TreeViewer(drillDown, SWT.NONE);
+			drillDown.setChildTree(treeViewer);
+			TemplatesContainerContentProvider cp = new TemplatesContainerContentProvider();
+			treeViewer.setContentProvider(cp);
+			treeViewer.setLabelProvider(new TigerstripeLabelProvider());
+			treeViewer.setComparator(new ViewerComparator());
+			treeViewer.setUseHashlookup(true);
+			treeViewer
+					.addSelectionChangedListener(new ISelectionChangedListener() {
+						public void selectionChanged(SelectionChangedEvent event) {
+							IStructuredSelection selection = (IStructuredSelection) event
+									.getSelection();
+							containerSelectionChanged((IContainer) selection
+									.getFirstElement());
+						}
+					});
+			treeViewer.addDoubleClickListener(new IDoubleClickListener() {
+				public void doubleClick(DoubleClickEvent event) {
+					ISelection selection = event.getSelection();
+					if (selection instanceof IStructuredSelection) {
+						Object item = ((IStructuredSelection) selection)
+								.getFirstElement();
+						if (item == null) {
+							return;
+						}
+						if (treeViewer.getExpandedState(item)) {
+							treeViewer.collapseToLevel(item, 1);
+						} else {
+							treeViewer.expandToLevel(item, 1);
+						}
+					}
+				}
+			});
+
+			treeViewer.setInput(ResourcesPlugin.getWorkspace());
+		}
+
+		public IContainer getSelectedContainer() {
+			return selectedContainer;
+		}
+
+		public void setInitialFocus() {
+			treeViewer.getTree().setFocus();
+		}
+
+		public void setSelectedContainer(IContainer container) {
+			selectedContainer = container;
+
+			List<IContainer> itemsToExpand = new ArrayList<IContainer>();
+			IContainer parent = container.getParent();
+			while (parent != null) {
+				itemsToExpand.add(0, parent);
+				parent = parent.getParent();
+			}
+			treeViewer.setExpandedElements(itemsToExpand.toArray());
+			treeViewer.setSelection(new StructuredSelection(container), true);
+		}
+
+		private class TemplatesContainerContentProvider implements
+				ITreeContentProvider {
+
+			public Object[] getChildren(Object element) {
+				if (element instanceof IWorkspace) {
+					List<IProject> result = new ArrayList<IProject>();
+					for (IProject project : ((IWorkspace) element).getRoot()
+							.getProjects()) {
+						try {
+							if (project.isOpen()
+									&& project
+											.getNature(GENERATOR_PROJECT_NATURE_ID) != null) {
+								result.add(project);
+							}
+						} catch (CoreException e) {
+							EclipsePlugin.log(e);
+						}
+					}
+					return result.toArray();
+				} else if (element instanceof IContainer) {
+					IContainer container = (IContainer) element;
+					if (container.isAccessible()) {
+						IResource[] members = null;
+
+						if (container instanceof IProject) {
+							IProject project = (IProject) container;
+							IResource member = project.getFolder(new Path(
+									"templates"));//$NON-NLS-1$
+							if (member != null) {
+								members = new IResource[] { member };
+							}
+						} else {
+							try {
+								members = container.members();
+							} catch (CoreException e) {
+								EclipsePlugin.log(e);
+							}
+						}
+
+						if (members != null) {
+							List<IResource> result = new ArrayList<IResource>();
+							for (IResource member : members) {
+								if (member.getType() != IResource.FILE) {
+									result.add(member);
+								}
+							}
+							return result.toArray();
+						}
+					}
+				}
+				return new Object[0];
+			}
+
+			public Object[] getElements(Object element) {
+				return getChildren(element);
+			}
+
+			public Object getParent(Object element) {
+				if (element instanceof IResource) {
+					return ((IResource) element).getParent();
+				}
+				return null;
+			}
+
+			public boolean hasChildren(Object element) {
+				return getChildren(element).length > 0;
+			}
+
+			public void inputChanged(Viewer viewer, Object oldInput,
+					Object newInput) {
+			}
+
+			public void dispose() {
+			}
+		}
+	}
 }
