fdec212cc948d4f65dcd8d4b3230df6301111fa5
jstrawn <jstrawn>
2011-02-03 22:08:16 +0000

    change to jstrawn for upload

--- upload-to.sh
+++ upload-to.sh
@@ -28,5 +28,5 @@ esac
  
 echo "Uploading build ${PROMOTED_BUILD} to $SITE_NAME"
 cd ${WORKSPACE}/../builds/${PROMOTED_BUILD}/archive/target/site
-scp -rv * edillon@download1.eclipse.org:"$SITE" 
-ssh edillon@download1.eclipse.org chmod -R 644 "$SITE"
+chmod -R 644 *
+scp -rv * jstrawn@download1.eclipse.org:"$SITE"
