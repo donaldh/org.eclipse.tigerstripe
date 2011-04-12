3674ec12b45fea3de2bf45604200bad10d46dc6b
djohnson <djohnson>
2011-04-12 22:16:04 +0000

    Making target out of date marker an information marker as requested by Chris

--- plugins/org.eclipse.tigerstripe.workbench.base/src/java/org/eclipse/tigerstripe/workbench/internal/core/model/ModelResourcesChangeListener.java
+++ plugins/org.eclipse.tigerstripe.workbench.base/src/java/org/eclipse/tigerstripe/workbench/internal/core/model/ModelResourcesChangeListener.java
@@ -66,6 +66,7 @@ public class ModelResourcesChangeListener implements ITigerstripeChangeListener 
                                 }
                             }
                         };
+                        job.setRule(folder);
                         job.schedule();
                     }
                 }
