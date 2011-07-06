89c492c3535c50234e89dce57eab807a3bd1b648
asalnik <asalnik>
2011-07-06 07:35:52 +0000

    bug 232727: validation expressions corrected

--- plugins/org.eclipse.tigerstripe.workbench.base/src/java/org/eclipse/tigerstripe/workbench/model/deprecated_/IPrimitiveTypeArtifact.java
+++ plugins/org.eclipse.tigerstripe.workbench.base/src/java/org/eclipse/tigerstripe/workbench/model/deprecated_/IPrimitiveTypeArtifact.java
@@ -16,16 +16,16 @@ public interface IPrimitiveTypeArtifact extends IAbstractArtifact,
 	public final static String RESERVED = "<reserved>";
 
 	public final static String[][] reservedPrimitiveTypes = {
-			{ "int", "Reserved int primitive Type.", "^[-+]?[0-9]*" },
+			{ "int", "Reserved int primitive Type.", "[+-]?[0-9]{1,10}" },
 			{ "char", "Reserved char primitive Type.", ".{1}" },
-			{ "boolean", "Reserved boolean primitive Type.", "1|0|true|false" },
-			{ "byte", "Reserved byte primitive Type.", "^[-+]?[0-9]*" },
+			{ "boolean", "Reserved boolean primitive Type.", "true|false" },
+			{ "byte", "Reserved byte primitive Type.", "[+-]?[0-9]{1,3}" },
 			{ "double", "Reserved double primitive Type.",
-					"^[-+]?[0-9]*\\.?[0-9]+$" },
+					"[-+]?[0-9]*\\.?[0-9]+" },
 			{ "float", "Reserved float primitive Type.",
-					"^[-+]?[0-9]*\\.?[0-9]+$" },
-			{ "long", "Reserved long primitive Type.", "^[-+]?[0-9]*" },
-			{ "short", "Reserved short primitive Type.", "^[-+]?[0-9]*" },
+					"[-+]?[0-9]*\\.?[0-9]+" },
+			{ "long", "Reserved long primitive Type.", "[+-]?[0-9]{1,19}" },
+			{ "short", "Reserved short primitive Type.", "[+-]?[0-9]{1,5}" },
 			{ "void", "Reserved void primitive Type.", null } };
 
 }
