88341cd9bd22405f766f444914ffce4cfb92f3a6
verastov <verastov>
2011-08-23 11:43:41 +0000

    Double clicking on a referenced module on one project will expand the referenced module on another project

--- plugins/org.eclipse.tigerstripe.workbench.ui.base/src/java/org/eclipse/tigerstripe/workbench/ui/internal/views/explorerview/TSExplorer.java
+++ plugins/org.eclipse.tigerstripe.workbench.ui.base/src/java/org/eclipse/tigerstripe/workbench/ui/internal/views/explorerview/TSExplorer.java
@@ -1,6 +1,12 @@
 package org.eclipse.tigerstripe.workbench.ui.internal.views.explorerview;
 
+import org.eclipse.jface.action.IAction;
+import org.eclipse.jface.viewers.DoubleClickEvent;
+import org.eclipse.jface.viewers.ITreeSelection;
+import org.eclipse.jface.viewers.TreePath;
+import org.eclipse.jface.viewers.TreeViewer;
 import org.eclipse.ui.navigator.CommonNavigator;
+import org.eclipse.ui.navigator.ICommonActionConstants;
 
 public class TSExplorer extends CommonNavigator {
 
@@ -9,4 +15,29 @@ public class TSExplorer extends CommonNavigator {
 		return "Tigerstripe Explorer";
 	}
 
+	@Override
+	protected void handleDoubleClick(DoubleClickEvent anEvent) {
+
+		if (!(anEvent.getSelection() instanceof ITreeSelection)) {
+			super.handleDoubleClick(anEvent);
+			return;
+		}
+
+		IAction openHandler = getViewSite().getActionBars()
+				.getGlobalActionHandler(ICommonActionConstants.OPEN);
+
+		if (openHandler == null) {
+			ITreeSelection selection = (ITreeSelection) anEvent.getSelection();
+
+			TreePath[] paths = selection.getPaths();
+
+			if (paths.length != 0) {
+				TreeViewer viewer = getCommonViewer();
+				if (viewer.isExpandable(paths[0])) {
+					viewer.setExpandedState(paths[0],
+							!viewer.getExpandedState(paths[0]));
+				}
+			}
+		}
+	}
 }
