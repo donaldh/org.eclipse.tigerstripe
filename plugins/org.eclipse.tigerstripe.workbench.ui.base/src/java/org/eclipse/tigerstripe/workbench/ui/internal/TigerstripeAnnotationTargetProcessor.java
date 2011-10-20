3bba787432515730cbf889ada6d863e54d05c171
asalnik <asalnik>
2011-10-20 05:43:50 +0000

    bug 355040: editor shouldn't be in dirty state while adding an annotation to a method argument

--- plugins/org.eclipse.tigerstripe.workbench.ui.base/src/java/org/eclipse/tigerstripe/workbench/ui/internal/TigerstripeAnnotationTargetProcessor.java
+++ plugins/org.eclipse.tigerstripe.workbench.ui.base/src/java/org/eclipse/tigerstripe/workbench/ui/internal/TigerstripeAnnotationTargetProcessor.java
@@ -4,13 +4,19 @@ import org.eclipse.core.resources.IResource;
 import org.eclipse.tigerstripe.annotation.ui.core.ITargetProcessor;
 import org.eclipse.tigerstripe.workbench.model.deprecated_.IAbstractArtifact;
 import org.eclipse.tigerstripe.workbench.model.deprecated_.IMember;
+import org.eclipse.tigerstripe.workbench.model.deprecated_.IMethod.IArgument;
 import org.eclipse.tigerstripe.workbench.ui.internal.utils.Editors;
 import org.eclipse.tigerstripe.workbench.utils.AdaptHelper;
 import org.eclipse.ui.IEditorPart;
 
 public class TigerstripeAnnotationTargetProcessor implements ITargetProcessor {
 
-	public boolean isDirty(Object object) {
+	public boolean isDirty(Object input) {
+		Object object = input;
+		if (object instanceof IArgument) {
+			object = ((IArgument) object).getContainingMethod();
+		}
+
 		if (object instanceof IMember) {
 			IMember m = (IMember) object;
 			IAbstractArtifact art = m.getContainingArtifact();
@@ -31,7 +37,11 @@ public class TigerstripeAnnotationTargetProcessor implements ITargetProcessor {
 		return false;
 	}
 
-	public String getName(Object object) {
+	public String getName(Object input) {
+		Object object = input;
+		if (object instanceof IArgument) {
+			object = ((IArgument) object).getContainingMethod();
+		}
 		if (object instanceof IMember) {
 			IMember member = (IMember) object;
 			return member.getMemberName() + " " + member.getUniqueName();
@@ -39,7 +49,11 @@ public class TigerstripeAnnotationTargetProcessor implements ITargetProcessor {
 		return null;
 	}
 
-	public String getDirtyViolationMessage(Object object) {
+	public String getDirtyViolationMessage(Object input) {
+		Object object = input;
+		if (object instanceof IArgument) {
+			object = ((IArgument) object).getContainingMethod();
+		}
 		if (object instanceof IMember) {
 			IAbstractArtifact containingArtifact = ((IMember) object).getContainingArtifact();
 			StringBuilder msg = new StringBuilder(getName(object));
