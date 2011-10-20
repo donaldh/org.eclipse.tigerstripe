56e751a4338c275e1660de3a08f68f9d914e24d1
djohnson <djohnson>
2011-10-20 23:00:07 +0000

    Removing unneeded org.eclipse.tigerstripe.espace plugins. Refactoring pom to use global -DskipTests to skip tests instead of custom -Dskip.tests

--- plugins/org.eclipse.tigerstripe.espace.resources/src/org/eclipse/tigerstripe/espace/resources/IResourceChangeListenerProvider.java
+++
@@ -1,9 +0,0 @@
-package org.eclipse.tigerstripe.espace.resources;
-
-import org.eclipse.core.resources.IResourceChangeListener;
-
-public interface IResourceChangeListenerProvider {
-
-	IResourceChangeListener get();
-	
-}
