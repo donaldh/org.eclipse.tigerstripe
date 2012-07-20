03b01aa5d9aceb4bbcc45217ddb542e4169e5095
verastov <verastov>
2012-07-20 10:49:28 +0000

    Start on Headless mode

--- plugins/org.eclipse.tigerstripe.workbench.base/src/java/org/eclipse/tigerstripe/workbench/spellcheck/SpellingCore.java
+++ plugins/org.eclipse.tigerstripe.workbench.base/src/java/org/eclipse/tigerstripe/workbench/spellcheck/SpellingCore.java
@@ -3,9 +3,14 @@ package org.eclipse.tigerstripe.workbench.spellcheck;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.Platform;
+import org.eclipse.core.runtime.Status;
 import org.eclipse.core.runtime.content.IContentTypeManager;
 import org.eclipse.jface.text.Document;
+import org.eclipse.tigerstripe.workbench.internal.BasePlugin;
+import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.editors.text.EditorsUI;
 import org.eclipse.ui.texteditor.spelling.ISpellingProblemCollector;
 import org.eclipse.ui.texteditor.spelling.SpellingContext;
 import org.eclipse.ui.texteditor.spelling.SpellingProblem;
@@ -18,8 +23,22 @@ public class SpellingCore {
 
 	static {
 		textContext = new SpellingContext();
-		textContext.setContentType(Platform.getContentTypeManager().getContentType(IContentTypeManager.CT_TEXT));
-		spellingService = new ValidationSpellingService();
+		textContext.setContentType(Platform.getContentTypeManager()
+				.getContentType(IContentTypeManager.CT_TEXT));
+		spellingService = isHeadlessMode() ? new ValidationSpellingService()
+				: EditorsUI.getSpellingService();
+	}
+
+	private static boolean isHeadlessMode() {
+		try {
+			PlatformUI.getWorkbench();
+			return true;
+		} catch (IllegalStateException ex) {
+			IStatus status = new Status(IStatus.INFO, BasePlugin.PLUGIN_ID,
+					"Start on Headless mode");
+			BasePlugin.getDefault().getLog().log(status);
+			return false;
+		}
 	}
 
 	public static List<String> checkText(String text) {
@@ -30,8 +49,8 @@ public class SpellingCore {
 
 	public static class ProblemCollector implements ISpellingProblemCollector {
 
-		private final List<String> errors = new ArrayList<String>(); 
-		
+		private final List<String> errors = new ArrayList<String>();
+
 		public ProblemCollector() {
 			super();
 		}
